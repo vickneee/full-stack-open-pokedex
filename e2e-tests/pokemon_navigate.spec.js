0f2d70f9cf9bd6cc4b21b1a33abd580a591ca336
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-01 01:24:15 +0300

    Add Fix 11.9 Simple end-to-end tests

--- e2e-tests/pokemon_navigate.spec.js
+++ e2e-tests/pokemon_navigate.spec.js
@@ -1,16 +1,11 @@
-// // eslint-disable-next-line no-undef
-// const { test, describe, expect } = require('@playwright/test')
-//
-// describe('Pokedex', () => {
-//   test('pokemon page can be opened and navigated to page where is word chorofyll', async ({ page }) => {
-//     await page.goto('')
-//     await page.getByText('bulbasaur').click()
-//     await expect(page.getByText('chlorophyll')).toBeVisible()
-//     await page.getByText('Next').click()
-//     await expect(page.getByText('ivysaur')).toBeVisible()
-//     await page.getByText('Next').click()
-//     await expect(page.getByText('venusaur')).toBeVisible()
-//     await page.getByText('Previous').click()
-//     await expect(page.getByText('ivysaur')).toBeVisible()
-//   })
-// })
+// eslint-disable-next-line no-undef
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('from main pokemon page navigate to page where is word chlorophyll', async ({ page }) => {
+    await page.goto('')
+    await page.click('a[href="/pokemon/ivysaur"]')
+    await expect(page.getByText('Chlorophyll')).toBeVisible()
+    // await expect(page.getByText('Chllllllorophyll')).toBeVisible() // this test should fail
+  })
+})

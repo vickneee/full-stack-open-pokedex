4672d54f2bb4cd76ce5064f67de85529a224eaac
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-01 01:36:56 +0300

    Add 11.9 Simple end-to-end tests

--- e2e-tests/pokemon_navigate.spec.js
+++ e2e-tests/pokemon_navigate.spec.js
@@ -3,7 +3,7 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('from main pokemon page navigate to page where is word chlorophyll', async ({ page }) => {
-    await page.goto('')
+    await page.goto('/')
     await page.click('a[href="/pokemon/ivysaur"]')
     await expect(page.getByText('chlorophyll')).toBeVisible() // Lowercase 'c' in the source code
     // await expect(page.getByText('chllllllorophyll')).toBeVisible() // this test should fail
